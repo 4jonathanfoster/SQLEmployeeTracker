93643582935a1061550d156e50b704d9b3e9a253
4jonathanfoster <82682840+4jonathanfoster@users.noreply.github.com>
2021-08-12 18:19:36 -0500

    seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,45 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
+DROP DATABASE IF EXISTS team_db;
 
-USE company_db;
+CREATE DATABASE team_db;
+
+USE team_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+
+id INT NOT NULL AUTO_INCREMENT,
+
+name VARCHAR(30) NOT NULL,
+
+PRIMARY KEY(id)
+
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+
+id INT NOT NULL AUTO_INCREMENT,
+
+title VARCHAR(30) NOT NULL,
+
+salary DECIMAL(10,2) NOT NULL,
+
+department_id INT NOT NULL,
+
+PRIMARY KEY (id)
+
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+id INT NOT NULL AUTO_INCREMENT,
+
+first_name VARCHAR(30) NOT NULL,
+
+last_name VARCHAR(30) NOT NULL,
+
+role_id INT NOT NULL,
+
+manager_id INT,
+
+PRIMARY KEY (id)
+
 );
