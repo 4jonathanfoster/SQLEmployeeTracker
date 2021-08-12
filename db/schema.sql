89aeed70aa4863b552f8b9e12eaf9a2fedb36780
4jonathanfoster <82682840+4jonathanfoster@users.noreply.github.com>
2021-08-12 18:52:03 -0500

    seeds up date

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS team_db;
+DROP DATABASE IF EXISTS employees_db;
 
-CREATE DATABASE team_db;
+CREATE DATABASE employees_db;
 
-USE team_db;
+USE employees_db;
 
 CREATE TABLE department (
 
