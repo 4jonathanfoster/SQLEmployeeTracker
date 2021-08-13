a33e455d60d64b9060645a1a8ca17a7dcbc4841a
4jonathanfoster <82682840+4jonathanfoster@users.noreply.github.com>
2021-08-12 19:57:16 -0500

    functions

--- index.js
+++ index.js
@@ -18,52 +18,101 @@ db.connect(function(err) {
 
 function mainMenuOnStart() {
     inquirer.prompt([{
-        type: "list",
-        name: "choice",
-        message: "What would you like to do?",
-        choices: [
-            "View Departments",
-            "View Roles",
-            "Add Department"
-        ]
-    }]).then(({ choice }) => {
-        console.log(choice)
-        if (choice == "View Departments") {
-            viewDept()
-        }
+            type: "list",
+            choices: [
+                "Add department",
+                "Add roles",
+                "Add employee",
+                "View departments",
+                "View roles",
+                "View employees",
+                "Update employee role",
+                "Quit"
+            ],
+            message: "What would you like to do?",
+            name: "option"
+        })
+        .then(function(result) {
+            console.log("You entered: " + result.option);
 
-        if (choice == "Add Department") {
-            addDept()
-        }
-    })
-}
+            switch (result.option) {
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Add roles":
+                    addRole();
+                    break;
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "View departments":
+                    viewDepartment();
+                    break;
+                case "View roles":
+                    viewRoles();
+                    break;
+                case "View employees":
+                    viewEmployees();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+                default:
+                    quit();
+            }
+        });
+    }
 
-function viewDept() {
-    const sqlString = `SELECT * FROM department`
 
-    connection.query(sqlString, (err, data) => {
-        if (err) throw err;
-        console.log("\n")
-        console.table(data)
-        console.log("\n")
+    //All of the corresponding functions found below
 
-        mainMenuOnStart()
-    })
-}
+    function addDepartment() {
 
-function addDept() {
-    inquirer.prompt([{
-        type: "input",
-        name: "newDept",
-        message: "What is the new department's name?"
-    }]).then(({ newDept }) => {
-        const sqlString = `
+
+        inquirer.prompt({
+
+            type: "input",
+            message: "What is the name of the department?",
+            name: "deptName"
+
+        }).then(function(answer) {
+
+
+
+            db.query("INSERT INTO department (name) VALUES (?)", [answer.deptName], function(err, res) {
+                if (err) throw err;
+                console.table(res)
+                startScreen()
+            })
+        })
+    }
+
+    function viewDept() {
+        const sqlString = `SELECT * FROM department`
+
+        connection.query(sqlString, (err, data) => {
+            if (err) throw err;
+            console.log("\n")
+            console.table(data)
+            console.log("\n")
+
+            mainMenuOnStart()
+        })
+    }
+
+    function addDept() {
+        inquirer.prompt([{
+            type: "input",
+            name: "newDept",
+            message: "What is the new department's name?"
+        }]).then(({ newDept }) => {
+            const sqlString = `
         INSERT INTO department(name)
         VALUES (?)`
 
-        connection.query(sqlString, [newDept], (err, data) => {
-            if (err) throw err;
-            mainMenuOnStart()
+            connection.query(sqlString, [newDept], (err, data) => {
+                if (err) throw err;
+                mainMenuOnStart()
+            })
         })
-    })
-}
+    }
